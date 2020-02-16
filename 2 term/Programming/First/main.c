fd9f965b1e7abaf3f8e629f4123c81154b82a94b
fedjaz <fedjaz5@gmail.com>
2020-02-16 22:34:24 +0300

    Done 1 task

---
+++ 2 term/Programming/First/main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <math.h>
+
+int getAns(long long n, int k){
+    while(k--){
+        n /= 10;
+    }
+    return n % 10;
+}
+int main(){
+
+    //28
+    long long a = 1, b = 1, c;
+    int len = 2, k;
+    scanf("%d", &k);
+    if(k <= 2){
+        printf("%d", 1);
+        return 0;
+    }
+    for(int i = 0; i < 30; i++){
+        c = a + b;
+        a = b;
+        b = c;
+        len += floor(log10(c)) + 1;
+        if(k <= len){
+            printf("%d\n", getAns(c, len - k));
+            return 0;
+        }
+    }
+    printf("%s", "K is too big!");
+    return 0;
+}
