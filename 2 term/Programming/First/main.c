32373baa1e158ca161b86d3c99dda6f46cc03533
fedjaz <fedjaz5@gmail.com>
2020-02-21 13:10:45 +0300

    added BigInt

--- 2 term/Programming/First/main.c
+++ 2 term/Programming/First/main.c
@@ -1,30 +1,78 @@
 #include <stdio.h>
 #include <math.h>
+#include <stdlib.h>
 
-int getAns(long long n, int k){
-    while(k--){
-        n /= 10;
-    }
-    return n % 10;
+#define N 100
+typedef struct{
+    int num[N];
+    int len;
+} bigInt;
+
+int max(int a, int b){
+    if(a > b)
+        return a;
+    return b;
+
 }
+bigInt * newBigInt(int n){
+    bigInt * a = malloc(sizeof(bigInt));
+    a->len = 0;
+    int i;
+    for(i = 0; i < N; i++){
+        a->num[i] = 0;
+    }
+    i = 0;
+    while(n != 0){
+        a->num[i] = n % 10;
+        n /= 10;
+        i++;
+        a->len++;
+    }
+    return a;
+}
+
+bigInt * add(bigInt * a, bigInt * b){
+    int i = 0;
+    bigInt * c = newBigInt(0);
+
+    for(i = 0; i < max(a->len, b->len); i++){
+        int tmp = a->num[i] + b->num[i];
+        c->num[i] += tmp;
+        c->num[i + 1] += c->num[i] / 10;
+        c->num[i] %= 10;
+        c->len++;
+    }
+    if(c->num[c->len] != 0){
+        c->len++;
+    }
+    return c;
+}
+
+void printBigInt(bigInt * a){
+    int i;
+    for(i = a->len - 1; i >= 0; i--){
+        printf("%d", a->num[i]);
+    }
+}
+
 int main(){
-    long long a = 1, b = 1, c, i;
-    int len = 2, k;
+    bigInt * a = newBigInt(1), * b = newBigInt(1), * c;
+    int len = 2, k, i;
     scanf("%d", &k);
     if(k <= 2){
         printf("%d", 1);
         return 0;
     }
-    for(i = 0; i < 30; i++){
-        c = a + b;
+    while(a->len < 1e5){
+        c = add(a, b);
+        free(a); 
         a = b;
         b = c;
-        len += floor(log10(c)) + 1;
+        len += c->len;
         if(k <= len){
-            printf("%d\n", getAns(c, len - k));
+            printf("%d\n", c->num[len - k]);
             return 0;
         }
     }
-    printf("%s", "K is too big!");
     return 0;
 }
