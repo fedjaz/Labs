18f9b59d1c11c41b40f98236309a7168ccd37988
Федор Юрецкий <fedjaz5@gmail.com>
2020-03-05 13:49:59 +0300

    done v2 from second in PT

---
+++ 2 term/PT/second/V2/V2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace V2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] s = Console.ReadLine().Split(' ');
+            s = s.Where(substr => substr != "").Reverse().ToArray();
+            Console.WriteLine(string.Join(" ", s));
+            Console.ReadKey();
+        }
+    }
+}
