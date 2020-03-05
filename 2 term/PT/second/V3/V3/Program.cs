d7416837f98beada2d693eded7ff4fafe643aa37
Федор Юрецкий <fedjaz5@gmail.com>
2020-03-05 16:22:57 +0300

    done v3

---
+++ 2 term/PT/second/V3/V3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace V3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            var arr = s.Split(' ').Select(sb => sb.ToUpper()).Where(sub => sub != "" && IsHex(sub));
+            arr = arr.Select(sb => int.Parse(sb, System.Globalization.NumberStyles.HexNumber).ToString());
+            arr.ToList().ForEach(sb => Console.Write(sb + " "));
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+
+        static bool IsHex(string s)
+        {
+            foreach(char c in s)
+            {
+                if(!((c >= 'A' && c <= 'F') || (c >= '0' && c <= '9'))){
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
