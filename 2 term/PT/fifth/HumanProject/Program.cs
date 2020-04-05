4ccd17a8c7c5327593343fa765c375108d8f126c
Федор Юрецкий <fedjaz5@gmail.com>
2020-04-05 21:26:11 +0300

    done fifth lab

---
+++ 2 term/PT/fifth/HumanProject/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HumanProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Athlete> gym = new List<Athlete>()
+            {
+                new SoccerPlayer(10, "Barselona",
+                                 SoccerPlayer.Positions.Forward,
+                                 100, 100, 100, 100,
+                                 "Lionel", "Messi"),
+                new Boxer(Boxer.Hands.Right, 80,
+                          100, 50, 100, 100, 100,
+                          "Mike", "Tyson"),
+                new Runner(9.58f, "Jamaica",
+                           50, 100, 100, 100,
+                           "Usain", "Bolt")
+            };
+
+            foreach(Athlete athlete in gym)
+            {
+                Console.WriteLine("Before training: ");
+                athlete.PrintInformation();
+                athlete.Train(1);
+                Console.WriteLine("\nAfter: ");
+                athlete.PrintInformation();
+                Console.WriteLine("__________________________");
+            }
+        }
+    }
+}
---
+++ 2 term/PT/fifth/HumanProject/Runner.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HumanProject
+{
+    class Runner : Athlete
+    {
+        public float PersonalRecord { get; set; }
+        public string CountryName { get; set; }
+
+        public Runner(float personalRecord, string countryName,
+                            float strength, float agility, float stamina,
+                            float skill)
+                            : base(strength, agility, stamina, skill)
+        {
+            PersonalRecord = personalRecord;
+            CountryName = countryName;
+        }
+
+        public Runner(float personalRecord, string countryName,
+                            float strength, float agility, float stamina,
+                            float skill, string name, string surname,
+                            bool isAlive = true)
+                            : base(strength, agility, stamina, skill,
+                                   name, surname, isAlive)
+        {
+            PersonalRecord = personalRecord;
+            CountryName = countryName;
+        }
+
+        public Runner(float personalRecord, string countryName,
+                            float strength, float agility, float stamina,
+                            float skill, string name, string surname,
+                            DateTime dateOfBirth, Genders gender,
+                            string adress = null, bool isAlive = true)
+                            : base(strength, agility, stamina, skill,
+                                   name, surname, dateOfBirth, gender,
+                                   adress, isAlive)
+        {
+            PersonalRecord = personalRecord;
+            CountryName = countryName;
+        }
+
+        public override void Train(int time)
+        {
+            Skill += time * 0.2f;
+            Stamina += time * 0.2f;
+            Agility += time * 0.2f;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder output = new StringBuilder(base.ToString());
+            output.AppendLine();
+            output.AppendLine(string.Format("Personal record: {0} seconds", PersonalRecord));
+            output.Append(string.Format("Country: {0}", CountryName));
+            return output.ToString();
+        }
+    }
+}
