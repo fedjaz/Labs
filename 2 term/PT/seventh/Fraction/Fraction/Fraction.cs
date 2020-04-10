c4632b2a323d413763447fc3b462a3fd0e745ed8
Федор Юрецкий <fedjaz5@gmail.com>
2020-04-10 21:24:33 +0300

    removed unused namespaces

--- 2 term/PT/seventh/Fraction/Fraction/Fraction.cs
+++ 2 term/PT/seventh/Fraction/Fraction/Fraction.cs
@@ -1,11 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using System.Runtime.Remoting.Messaging;
-using System.Text;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 
 namespace Fraction
 {
