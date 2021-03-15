e9da3f9433da74fff9f431de25376f07c60f62f4
fedjaz <fedjaz5@gmail.com>
2021-03-15 21:50:50 +0300

    done ca lab1

---
+++ 4 term/CA/First/First/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace First
+{
+    class Program
+    {
+        static void Main()
+        {
+            Integer a = new Integer(99, 8, true);
+            Integer b = new Integer(5, 8, true);
+
+            int c = int.MaxValue;
+            int d = 150;
+            int e = c + d;
+            unchecked
+            {
+                Console.WriteLine(a / b);
+            }
+        }
+    }
+}
