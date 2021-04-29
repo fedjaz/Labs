053644b1507f7e73336c9bcdc522b5a04e2cc6fc
fedjaz <fedjaz5@gmail.com>
2021-04-29 15:59:40 +0300

    done all and added interactive

--- 4 term/CA/Second/Second/Second/Program.cs
+++ 4 term/CA/Second/Second/Second/Program.cs
@@ -10,8 +10,9 @@ namespace Second
     {
         static void Main(string[] args)
         {
-            Decimal32 decimal32 = new Decimal32(-2754.8888888f);
-            Console.WriteLine(decimal32.ToFloat());
+            Decimal32 a = new Decimal32(-1f, true);
+            Decimal32 b = new Decimal32(-3f, true);
+            Console.WriteLine($"{(a / b).ToFloat():0.####}");
         }
     }
 }
