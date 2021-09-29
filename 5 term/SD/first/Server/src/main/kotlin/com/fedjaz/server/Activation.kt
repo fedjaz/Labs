48404c55f1c07cb2b6b3a3371f9412a594baec1b
fedjaz <fedjaz5@gmail.com>
2021-09-29 17:08:31 +0300

    fix: path

--- 5 term/SD/first/Server/src/main/kotlin/com/fedjaz/server/Activation.kt
+++ 5 term/SD/first/Server/src/main/kotlin/com/fedjaz/server/Activation.kt
@@ -1,14 +1,16 @@
 package com.fedjaz.server
 
 import java.security.KeyFactory
+import java.security.KeyPairGenerator
 import java.security.Signature
 import java.security.spec.PKCS8EncodedKeySpec
 import java.util.*
 
 class Activation {
-    private val privateKey = "MIICZQIBADCCAjkGByqGSM44BAEwggIsAoIBAQDR+2AaIELyMqxcHK1xqprr2rG7kvaD8BzUdqWEWLnkcuAiAy1Res5hPWNHb4w75Zh9B/9aQYhXzaQQkh3MUQRv7xpfo4vyoFIxM0pxnD6gzu24vTAU7PbVz2DzwpfGtz7/+L+rwNgqJq22MQKK1E3a6R8WzGWqOSSthYp7aIIm/g8eSwt89zhxVa8IvtGRUzHKdG2WRWtbyH36IG/dcNvh6pWyP84A10y2w/hKN3wNgWnNvL3+1E3QGRUaYUHUxd1t3/Ovj/HQwSxiGEFTwzodFXXM+q1jqSlmsqYVFwZfKyntzOYIZ4WNJexeWy/mm6TfsIffz8PiUsuZYfwdRzs9AiEAoVOiTMEt6cfBT5y5cAhuEDaX0I14GOTW8O7iL4Y1ufMCggEAeh/qT+NwLjKL75n36NW9afo4sTEqybJ5sY28XR8fZwKkaDebM/tad+wgD/ueaTiehLjWIgyoL5jDTgtzu5xTbiDSOa6KcrbF8ExhrVZjTY7//W0UdHcuJXoMklMSfP+7P8Wpdvw+p8NP5ovKEnm6LC7GysKiyHZkBjOvjkL3Kvm1p1HJNup4tTt3X8b5YZ9Oebd1ExoWSzDCvvtH11TqSuaKTf7MAi0ootGx1TMM8O2rSgTPkQOmos+4Qrp0rHZZ6SWmqhloWbHtitwvegT2J/GYdBzriJQqKoFjwUcmYXGZnl/xT1b43Ajh5Je/IeQwFMlMOVl+QNo9z7b3xEQJ7QQjAiEAjlQn+DHhRSYUOglnrfcJptIW3S8DAneCfMZ+W1Zjq/M="
+    private val privateKey = System.getenv("privateKey")
 
     fun createKey(deviceID: String) : String{
+        gen()
         val privateKeyBytes = Base64.getDecoder().decode(
             privateKey
         )
@@ -21,5 +23,19 @@ class Activation {
         signature.update(deviceID.toByteArray())
         val keyBytes = signature.sign()
         return Base64.getEncoder().encodeToString(keyBytes)
+    }
+
+    private fun gen(){
+        val keyPairGen = KeyPairGenerator.getInstance("DSA")
+        keyPairGen.initialize(2048)
+        val keys = keyPairGen.genKeyPair()
+
+        val privateKey = keys.private
+        val publicKey = keys.public
+
+        val privateString = Base64.getEncoder().encodeToString(privateKey.encoded)
+        val publicString = Base64.getEncoder().encodeToString(publicKey.encoded)
+        println(privateString)
+        println(publicString)
     }
 }
--- 5 term/SD/first/Server/src/main/kotlin/com/fedjaz/server/ActivationController.kt
+++ 5 term/SD/first/Server/src/main/kotlin/com/fedjaz/server/ActivationController.kt
@@ -5,7 +5,6 @@ import org.springframework.core.io.Resource
 import org.springframework.http.HttpHeaders
 import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
-import org.springframework.util.ResourceUtils
 import org.springframework.web.bind.annotation.*
 import java.io.File
 import java.io.FileInputStream
@@ -20,7 +19,7 @@ class ActivationController {
 
     @GetMapping("/download/full")
     fun downloadFull(): ResponseEntity<Resource>{
-        val file = File(System.getProperty("user.dir") + "\\src\\main\\resources\\static\\downloads\\calculator-full.apk")
+        val file = File(System.getProperty("user.dir") + "/src/main/resources/static/downloads/calculator-full.apk")
         val inputStreamResource = InputStreamResource(FileInputStream(file))
         val headers = HttpHeaders()
         headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=calculator-full.apk")
