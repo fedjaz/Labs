0f9e44b32b8bb43ea35b8f2d44a8fd6cc66c5c57
fedjaz <fedjaz5@gmail.com>
2021-10-08 17:27:01 +0300

    add: fourth lab

---
+++ 5 term/SCCS/fourth/WEB_953501_YURETSKI/WEB_953501_YURETSKI/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using WEB_953501_YURETSKI.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace WEB_953501_YURETSKI.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ 5 term/SCCS/fourth/WEB_953501_YURETSKI/WEB_953501_YURETSKI/Services/EmailConfimation.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+
+namespace WEB_953501_YURETSKI.Services
+{
+    public class EmailConfimation
+    {
+        public string CredentialEmail {  get; set; }
+        public string CredentialPassword {  get; set; }
+
+        public NetworkCredential networkCredential {  get; set; }
+        public EmailConfimation(string credentialEmail, string credentialPassword)
+        {
+            CredentialEmail = credentialEmail;
+            CredentialPassword = credentialPassword;
+            networkCredential = new NetworkCredential(credentialEmail, credentialPassword);
+        }
+
+        public string BeginConfirmation(string email)
+        {
+            string code = GenerateCode(32);
+            MailAddress from = new MailAddress(CredentialEmail);
+            MailAddress to = new MailAddress(email);
+            MailMessage message = new MailMessage(from, to);
+            message.Subject = "Подтверждение email";
+            StringBuilder body = new StringBuilder();
+            body.Append("<h2>Подтверждение email для WEB_953501_YURETSKI.");
+            body.Append("<br/>");
+            body.AppendLine($"<a href=\"https://localhost:44380/Account/Confirm?email={email}&code={code}\">Нажмите, чтобы подтвердить email</a>");
+            message.Body = body.ToString();
+            message.IsBodyHtml = true;
+
+            SmtpClient smtpClient = new SmtpClient("smtp.gmail.com", 587);
+            smtpClient.EnableSsl = true;
+            smtpClient.Credentials = networkCredential;
+            smtpClient.Send(message);
+
+            return code;
+        }
+
+        private string GenerateCode(int length)
+        {
+            StringBuilder buffer = new StringBuilder();
+            Random random = new Random();
+            for(int i  = 0; i < length; i++)
+            {
+                buffer.Append((char)random.Next('A', 'Z' + 1));
+            }
+            return buffer.ToString();
+        }
+    }
+}
