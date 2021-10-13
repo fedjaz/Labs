85ea71523510a287251020a29e9ff19cabfb214b
fedjaz <fedjaz5@gmail.com>
2021-10-13 15:27:44 +0300

    add: pager tag for sixth lab

---
+++ 5 term/SCCS/sixth/WEB_953501_YURETSKI/WEB_953501_YURETSKI/Entities/Food.cs
@@ -0,0 +1,14 @@
+﻿namespace WEB_953501_YURETSKI.Entities
+{
+    public class Food
+    {
+        public int Id {  get; set; }
+        public string Name {  get; set; }
+        public string Description {  get; set; }
+        public float Price {  get; set; }
+        public int CategoryId {  get; set; }
+        public Category Category {  get; set; }
+        public int ImageId {  get; set; }
+        public Image Image {  get; set; }
+    }
+}
